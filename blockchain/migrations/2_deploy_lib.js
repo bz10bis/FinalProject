1fde8a70569b357cc65ef60c52af6802b5bd34a1
bz10bis <baptistezegre@gmail.com>
2018-07-04 18:37:58 +0200

    remove erc20intf

--- blockchain/migrations/2_deploy_lib.js
+++ blockchain/migrations/2_deploy_lib.js
@@ -1,5 +1,5 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
-var ERC20 = artifacts.require("./ERC20interface.sol");
+//var ERC20 = artifacts.require("./ERC20interface.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
